85e2d783af4b90856094dd976ea20a7503915d27
MohammadRafik <rafik.mohammad1994@gmail.com>
2019-01-01 15:47:40 -0500

    created task table with model and controller to save tasks on the backend

---
+++ database/migrations/2018_12_29_192807_create_task_datas_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTaskDatasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('task_datas', function (Blueprint $table) {
+            $table->increments('Tableid');
+            $table->int('id');
+            $table->string('task');
+            $table->string('description');
+            $table->bigInteger('timeWorked');
+            $table->string('workDoneMessage');
+            $table->boolean('toggleMode');
+            $table->string('playAndPauseButtonSymbole');
+            $table->boolean('taskCompleted');
+            $table->string('color');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('task_datas');
+    }
+}
